4d08426dca1fdc0b765d4a2092255ea71659385f
Hưng Dõ <vokhanhhung03@gmail.com>
2024-10-29 16:11:24 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jewelry-shop/',
+  base: "/jewelry-shop/",
   plugins: [react()],
 })
