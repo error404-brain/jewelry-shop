6e71721acaa5d144b585cf7542d1f97ef4944c24
Hưng Dõ <vokhanhhung03@gmail.com>
2025-01-09 15:29:32 +0700

    rename

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './component/Header';
-import BodyContent from './component/main';
+import BodyContent from './component/homecontent';
 import Footer from './component/Footer';
 
 function App() {
